6b93e57e925be50862b0ae75184212961442a1a3
Vidoje Zeljic <zeljicvidoje@gmail.com>
2018-01-30 21:27:17 +0100

    022 / correction

---
+++ Spring1/src/com/vidojezeljic/spring1/TrackCoach.java
@@ -0,0 +1,14 @@
+package com.vidojezeljic.spring1;
+
+/**
+ *
+ * @author vidojezeljic
+ */
+public class TrackCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+}
