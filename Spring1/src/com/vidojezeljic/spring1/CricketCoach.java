dfa6e9377f8b9da847bd205d8d20119706fdf43f
Vidoje Zeljic <zeljicvidoje@gmail.com>
2018-01-30 23:52:28 +0100

    038

--- Spring1/src/com/vidojezeljic/spring1/CricketCoach.java
+++ Spring1/src/com/vidojezeljic/spring1/CricketCoach.java
@@ -4,9 +4,11 @@ package com.vidojezeljic.spring1;
  *
  * @author vidojezeljic
  */
-public class CricketCoach implements Coach{
+public class CricketCoach implements Coach {
 
     private FortuneService fortuneService;
+    private String emailAddress;
+    private String team;
 
     public CricketCoach() {
         System.out.println("CricketCoach: inside no-arg constructor");
@@ -15,6 +17,24 @@ public class CricketCoach implements Coach{
     public void setFortuneService(FortuneService fortuneService) {
         System.out.println("CricketCoach: inside setter method - setFortuneService");
         this.fortuneService = fortuneService;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("CricketCoach: inside setter method - setEmailAddress");
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("CricketCoach: inside setter method - setTeam");
+        this.team = team;
     }
 
     @Override
--- Spring1/src/com/vidojezeljic/spring1/SetterDemoApp.java
+++ Spring1/src/com/vidojezeljic/spring1/SetterDemoApp.java
@@ -15,11 +15,14 @@ public class SetterDemoApp {
                 new ClassPathXmlApplicationContext("applicationContext.xml");
         
         // retrieve bean from spring container
-        Coach coach = contex.getBean("myCricketCoach", Coach.class);
+        CricketCoach coach = contex.getBean("myCricketCoach", CricketCoach.class);
         
         // call method on the bean
         System.out.println(coach.getDailyWorkout());
         System.out.println(coach.getDailyFortune());
+        
+        System.out.println(coach.getEmailAddress());
+        System.out.println(coach.getTeam());
         
         // close the context
         contex.close();
