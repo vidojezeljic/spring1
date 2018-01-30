35c819dcc96a0069a4a13941ee55796d0a43d064
Vidoje Zeljic <zeljicvidoje@gmail.com>
2018-01-30 21:26:12 +0100

    022

--- Spring1/src/com/vidojezeljic/spring1/MyApp.java
+++ Spring1/src/com/vidojezeljic/spring1/MyApp.java
@@ -8,7 +8,7 @@ public class MyApp {
 
     public static void main(String[] args) {
 
-        Coach theCoach = new BaseballCoach();
+        Coach theCoach = new TrackCoach();
 
         System.out.println(theCoach.getDailyWorkout());
 
